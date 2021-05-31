4fab25dbc7755611448f74a40c47c7617351cb56
Jason J Cruz <jaycru06@yahoo.com>
2021-05-31 20:19:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 function findMinAndRemove(array){
-  // 
+  // Algorithm:
+  // find the minimum value in array 
+  // remove that value from the array 
+  // repeat this process for length of array 
+  
+  // Implementation:
+  // set a min and minIndex
+  // loop through array 
+  // compare min to current value
+  // update min if needed
+  // splice min from the array 
+  // return the min value 
+  let min = array[0];
+  let minIndex = 0;
+  
+  for (let idx = 0; idx < array.length; idx++) {
+    if (array[idx] < min) {
+      min = array[idx];
+      minIndex = idx;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function insertionSort(array){
